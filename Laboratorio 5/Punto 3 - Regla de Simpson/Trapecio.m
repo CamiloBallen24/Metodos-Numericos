b0c821ff4e2c5058f186bb81d09e6f65b88de583
jdacostabe <48451314+jdacostabe@users.noreply.github.com>
2019-11-17 21:53:10 -0500

    Update Trapecio.m

--- Laboratorio 5/Punto 3 - Regla de Simpson/Trapecio.m
+++ Laboratorio 5/Punto 3 - Regla de Simpson/Trapecio.m
@@ -2,17 +2,18 @@
 function T = Trapecio(f,a,b,M)
   % Se halla la longitud de cada intervalo
   h = (b-a)/M;
+  % Se declara la variable en la que se evaluará la funcion
   sum=0;
-  %variable en la que se evaluara la funcion
 
-  %Se halla la sumatoria de las funciones evaluada en numeros pares e impares
+  %Se halla la sumatoria de las funcion evaluada en todos los puntos del intervalo
   for k=1:M-1
      h1=a+k*h;
      sum += f(h1);
   endfor
 
-  %Se aplica la formula correspondiente
+  %Se aplica la fórmula correspondiente
   T = (h/2)*(f(a) + f(b)) + (h)*sum ;
+  %Hallamos la diferencia entre el resultado obtenido, y el valor real de la integral
   E = abs(T - quad(f,a,b));
   printf('Trapecio: %f \n', T);
   printf('Error: %f \n\n', E);
